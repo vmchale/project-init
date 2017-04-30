25dee9de399f5dcbd924e8a774c203ab456745ce
Vanessa McHale <tmchale@wisc.edu>
2017-04-29 23:26:40 -0500

    vim-example progress

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
-extern crate toml;
-extern crate clap;
-extern crate rustache;
 #[macro_use] extern crate serde_derive;
+#[macro_use] extern crate clap;
+
+extern crate toml;
+extern crate rustache;
 
 use rustache::{HashBuilder, Render};
 use toml::Value;
 use std::io::Cursor;
+use std::io::prelude::*;
+use std::fs::File;
+use clap::App;
 
 #[derive(Debug, Deserialize)]
 struct Author {
@@ -16,21 +20,41 @@ struct Author {
 #[derive(Debug, Deserialize)]
 struct Config {
     license: Option<String>,
+    version_control: Option<String>,
     author: Option<Author>,
 }
 
-fn main() {
-    // parse the toml
-    let toml_str = r#"
-        license = "BSD3"
+#[derive(Debug, Deserialize)]
+struct Directory {
+    files: Vec<String>,
+    directories: Vec<String>,
+    templates: Vec<String>,
+}
 
-        [author]
-        name = "vanessa"
-        email = "vamchale@gmail.com"
-    "#;
-    let decoded: Config = toml::from_str(toml_str).unwrap();
+fn main() {
+    // command-line parser
+    let yaml = load_yaml!("options-en.yml");
+    let matches = App::from_yaml(yaml).version(crate_version!()).get_matches();
+
+    // set path to .pi.toml
+    let mut path = std::env::home_dir().expect("Couldn't determine home directory.");
+    path.push(".pi.toml");
+
+    // read config file
+    let mut file = File::open(path)
+        .expect("File could not be read.");
+    let mut toml_str = String::new();
+    file.read_to_string(&mut toml_str)
+        .expect("File read failed");
+    let decoded: Config = toml::from_str(&toml_str).unwrap();
     println!("{:#?}", decoded);
     
+    // get directory
+    if let Some(command) = matches.subcommand_matches("send") {
+        let send_str = command.value_of("words")
+            .expect("Could not parse user input. Please check the string is correctly formatted");
+    }
+
     // Renders the given template string
     let data = HashBuilder::new().insert("name", "Vanessa McHale");
     let mut out = Cursor::new(Vec::new());
