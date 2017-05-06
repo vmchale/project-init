ad2cb3437781592d88ad6656c94d88ea78f0f40f
Vanessa McHale <tmchale@wisc.edu>
2017-05-06 01:33:03 -0500

    haskell improved

---
+++ examples/haskell/src/Lib.hs
@@ -0,0 +1,6 @@
+module Lib
+    ( exec
+    ) where
+
+exec :: IO ()
+exec = putStrLn "{{ project }} from template"
---
+++ examples/haskell/test/Spec.hs
@@ -0,0 +1,7 @@
+import Test.Hspec
+
+main :: IO ()
+main = hspec $ do
+    describe "head" $ do
+        parallel $ it "parses a .mad string with modifiers" $ do
+            head [1..] `shouldBe` 1
