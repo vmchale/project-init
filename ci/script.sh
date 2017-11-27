ddbd394a8c0ac737a1a2cc5140742ece6400da67
Vanessa McHale <vanessa.mchale@reconfigure.io>
2017-11-27 07:58:31 -0600

    fancy redox builds so bougie

--- ci/script.sh
+++ ci/script.sh
@@ -3,21 +3,30 @@
 set -ex
 
 main() {
-    cross build --target $TARGET
-    cross build --target $TARGET --release
+    if [ "$TARGET" = "x86_64-unknown-redox" ]; then
+        sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AA12E97F0881517F
+        sudo add-apt-repository 'deb https://static.redox-os.org/toolchain/apt /'
+        sudo apt update
+        sudo apt install x86-64-unknown-redox-gcc
+        rustup target add x86_64-unknown-redox
+        cargo build --target "$TARGET" --release
+    else
+        cross build --target "$TARGET"
+        cross build --target "$TARGET" --release
 
-    if [ ! -z $DISABLE_TESTS ]; then
-        return
+        if [ ! -z "$DISABLE_TESTS" ]; then
+            return
+        fi
+
+        cross test --target "$TARGET"
+        cross test --target "$TARGET" --release
+
+        cross run --target "$TARGET" -- a
+        cross run --target "$TARGET" --release -- a
     fi
-
-    cross test --target $TARGET
-    cross test --target $TARGET --release
-
-    # cross run --target $TARGET -- new idris proj
-    # cross run --target $TARGET --release -- new idris proj
 }
 
 # we don't run the "test phase" when doing deploys
-if [ -z $TRAVIS_TAG ]; then
+if [ -z "$TRAVIS_TAG" ]; then
     main
 fi
