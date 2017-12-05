34fd32773b53f04427abac4e879013a64ee46892
Vanessa McHale <vanessa.mchale@reconfigure.io>
2017-12-05 13:22:07 -0600

    ci attempt some stuff

--- ci/script.sh
+++ ci/script.sh
@@ -21,6 +21,9 @@ main() {
         cross test --target "$TARGET"
         cross test --target "$TARGET" --release
 
+        cross run --target "$TARGET" -- new haskell project
+        cross run --target "$TARGET" --release -- new haskell project -f
+
     fi
 }
 
