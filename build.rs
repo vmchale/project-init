2d38a59ac91aed3d72f94fc7409a6c67a883060b
Vanessa McHale <vamchale@gmail.com>
2020-04-02 09:35:53 -0500

    clippy

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ extern crate cli_setup;
 
 use cli_setup::*;
 
-pub const MAN_PI: &'static str = include_str!("man/pi.1");
+pub const MAN_PI: &str = include_str!("man/pi.1");
 
 fn main() {
     setup_manpages(MAN_PI, "pi");
